dd73d8fa04727482b3322acfc40577e9b3e8ea0e
Fredrik Leemann <fredrik@leemann.se>
2019-05-17 10:32:19 +0200

    work on paddles using matter

--- scenes/Pinball/Pinball.js
+++ scenes/Pinball/Pinball.js
@@ -4,8 +4,6 @@ class Pinball extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('flipperRight', 'assets/sprites/flipper-right.png');
-        this.load.image('flipperLeft', 'assets/sprites/flipper-left.png');
         this.load.json('shapes', 'assets/sprites/flipper_shapes.json');
         this.load.atlas('sheet', 'assets/sprites/flippers.png', 'assets/sprites/flippers.json')
         this.load.image('ball', 'assets/sprites/ball.png');
@@ -14,40 +12,24 @@ class Pinball extends Phaser.Scene {
     create() {
         const shapes = this.cache.json.get('shapes');
         gameState.cursors = this.input.keyboard.createCursorKeys();
+
+        this.matter.world.setBounds(0, 0, 400, 800, 1, true, true, true, false);
         
-        gameState.scoreBoard = this.add.rectangle(0, 0, 400, 80, 0x555555)
-        .setOrigin(0,0);
+        gameState.ball = this.matter.add.image(100, 200, 'ball')
+        .setBounce(0.96)
+        .setCircle()
 
-        gameState.ballLauncherWall = this.add.rectangle(370, 500, 2, 500, 0xFFFFFF)
-
-        gameState.scoreCircleT1 = this.add.circle(165, 165, 20, 0xFFFFFF);
-        gameState.scoreCircleR2 = this.add.circle(245, 225, 20, 0xFFFFFF);
-        gameState.scoreCircleL3 = this.add.circle(85, 225, 20, 0xFFFFFF);
-        gameState.scoreCircleB4 = this.add.circle(165, 300, 20, 0xFFFFFF);
-
-        //gameState.ball = this.matter.add.image(100, 200, 'ball')
-        //.setOrigin(0, 0)
-        //.setBounceY(2)
-
-        //gameState.rightFlipper = this.matter.add.image(340, 703, 'flipperRight')
         gameState.rightFlipper = this.matter.add.sprite(300, 720, 'sheet', 'flipperRight', {shape: shapes.flipper_right})
         .setIgnoreGravity(true)
-        .setInteractive()
-        //.setOrigin(1, 0)
-        .setDensity(50)
-        .setMass(50)
-        .setCollidesWith(gameState.ball);
+        // .setInteractive()
+        // .setDensity(50)
+        // .setMass(50)
 
-        //gameState.leftFlipper = this.matter.add.image(40, 700, 'flipperLeft')
         gameState.leftFlipper = this.matter.add.sprite(80, 720, 'sheet', 'flipperLeft', {shape: shapes.flipper_left})
         .setIgnoreGravity(true)
-        .setInteractive()
-        //.setOrigin(0, 0)
-        .setDensity(50)
-        .setMass(50)
-        .setCollidesWith(gameState.ball);
-
-        gameState.ball.circle(100, 200, 20, {render: {fillStyle: blueColor}})
+        // .setInteractive()
+        // .setDensity(50)
+        // .setMass(50)
     }
 
     update() {
