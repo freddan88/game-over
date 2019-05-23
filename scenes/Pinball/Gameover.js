b5b600512182968caed8118ed894aa1403f29d56
Isa Areschoug <isaar@Isas-MacBook-Air.local>
2019-05-23 14:44:19 +0200

    Add logic to gameover scene

--- scenes/Pinball/Gameover.js
+++ scenes/Pinball/Gameover.js
@@ -4,23 +4,34 @@ class Gameover extends Phaser.Scene {
   }
 
   create(){
-
     gameState.gameOverMessage = this.add.text(200, 300, 'Game Over!', {
       font: "40px",
       align: 'center',
     });
     gameState.gameOverMessage.setOrigin(0.5);
 
-    gameState.playAgainMessage = this.add.text(200, 400, 'Press _enter_ to play again', {
+    gameState.finalScore = this.add.text(200, 340, 'You got ' + gameState.score + ' points', {
+      font: "15px",
+      align: 'center',
+    });
+    gameState.finalScore.setOrigin(0.5);
+
+    gameState.playAgainMessage = this.add.text(200, 400, 'Press _space_ to play again', {
       font: "15px",
       align: 'center',
     });
     gameState.playAgainMessage.setOrigin(0.5);
+
+    gameState.cursors = this.input.keyboard.createCursorKeys();
+
   }
 
   update(){
-    if (this.cursors.enter.isDown) {
-      gameState.scene.start('Pinball');
+    if (gameState.cursors.space.isDown){
+      window.location.reload();
+      // gameState.gameOver = false;
+      // this.scene.start('Pinball');
+      // gameState.score = 0;
     }
   }
 
